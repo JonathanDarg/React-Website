7c13b611d0be132a87dfb9d253a6fc7ec785b565
jonathanDargakis <jmdargakis@loyola.edu>
2025-04-14 19:05:48 -0400

    bottom home page card

---
+++ src/components/Cards.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import './Cards.css'
+import Carditem from './Carditem'
+
+function Cards() {
+  return (
+    <div className="cards">
+        <h1>Check out these EPIC Destinations!</h1>
+        <div className="cards__container">
+            <div className="cards__wrapper">
+            <ul className="cards__items">
+              <Carditem
+                src='images/img-9.jpg'
+                text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                label='Adventure'
+                path='/about'
+              />
+              <Carditem
+                src='images/img-2.jpg'
+                text='Travel through the Islands of Bali in a Private Cruise'
+                label='Luxury'
+                path='/about'
+              />
+              <Carditem
+                src='images/img-3.jpg'
+                text='Fish in the Maldives while relaxing in a luxury spa resort'
+                label='Tranquility'
+                path='/about'
+              />
+            </ul>
+            {/* second row of cards 
+            <ul className="cards__items">
+              <Carditem
+                src='images/img-9.jpg'
+                text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                label='Adventure'
+                path='/about'
+                />
+                ... more cards
+            </ul>
+            */}
+            
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Cards
