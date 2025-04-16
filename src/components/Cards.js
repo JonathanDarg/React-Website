9671387dfef41c65a20996b9515577d1d6c910ee
jonathanDargakis <jmdargakis@loyola.edu>
2025-04-15 22:00:02 -0400

    seond row of cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,7 @@ import Carditem from './Carditem'
 function Cards() {
   return (
     <div className="cards">
-        <h1>Check out these EPIC Destinations!</h1>
+        <h1>WELCOME TO MY WEBSITE</h1>
         <div className="cards__container">
             <div className="cards__wrapper">
             <ul className="cards__items">
@@ -28,18 +28,27 @@ function Cards() {
                 path='/about'
               />
             </ul>
-            {/* second row of cards 
+            {/* second row of cards */}
             <ul className="cards__items">
               <Carditem
                 src='images/img-9.jpg'
                 text='Explore the hidden waterfall deep inside the Amazon Jungle'
                 label='Adventure'
                 path='/about'
-                />
-                ... more cards
+              />
+              <Carditem
+                src='images/img-2.jpg'
+                text='Travel through the Islands of Bali in a Private Cruise'
+                label='Luxury'
+                path='/about'
+              />
+              <Carditem
+                src='images/img-3.jpg'
+                text='Fish in the Maldives while relaxing in a luxury spa resort'
+                label='Tranquility'
+                path='/about'
+              />
             </ul>
-            */}
-            
             </div>
         </div>
       
