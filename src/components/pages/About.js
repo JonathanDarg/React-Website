f3f260cb48226a59ebf67b543c474673eaa957a3
jonathanDargakis <jmdargakis@loyola.edu>
2026-08-09 16:14:38 -0400

    updated packages

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,7 +9,7 @@ export default function About() {
                 <img src={`${process.env.PUBLIC_URL}/images/animated_me.png`} alt="animated me" className="about-anim" />
                 <div className="about-text">
                     <h1>About</h1>
-                    <p>Welcome — this is the about page.</p>
+                    <p>Hello! My name is Jonathan Dargakis and I am currently a senior studying Computer Science at Loyola Univeristy Maryland.</p>      
                 </div>
             </div>
         </div>
